aa515252e4f984797dda321ff0698c32012c78bc
kelvin <56135351+devchow@users.noreply.github.com>
2022-09-19 00:56:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# getx_image_picker
+ pick image from camera or gallery using Getx
---
+++ android/app/src/main/kotlin/com/example/getx_image_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_image_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
